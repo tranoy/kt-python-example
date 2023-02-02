fb1a8d1123511fba771f3c33e969fd77c8226ccc
tranoy <38881159+tranoy@users.noreply.github.com>
2023-02-02 17:16:28 +0900

    Q4 answer

--- Q4 answer.py
+++ Q4 answer.py
@@ -3,12 +3,18 @@ data = input('숫자로 이루어진 문자열을 입력하세요. ')
 size = len(data)
 result = 1
 for i in range(size):
-    if data[i] == '0':
-        #result = result + int(data[i])
-        continue
-    elif data[i] =='1':
-        result = result + int(data[i])
+    if i < size-1:
+        if data[i] == '0' or data[i] == '1':
+            result = result + int(data[i])
+            print(f'{data[i]} + ', end='')
+        else:
+            result = result * int(data[i])
+            print(f'{data[i]} * ', end='')
     else:
-        result = result * int(data[i])
-
+        if data[i] == '0' or data[i] == '1':
+            result = result + int(data[i])
+            print(f'{data[i]} = ', end='')
+        else:
+            result = result * int(data[i])
+            print(f'{data[i]} = ', end='')
 print(result)
